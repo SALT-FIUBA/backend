4fdf4b53fbde29ef5485eb927c4e0caf2f04b75a
matiassambrizzi <msambrizzi@fi.uba.ar>
2025-02-25 23:04:40 -0300

    update: Expose reservation api rest

--- src/main/kotlin/io/kauth/service/iotdevice/IoTDeviceEventHandler.kt
+++ src/main/kotlin/io/kauth/service/iotdevice/IoTDeviceEventHandler.kt
@@ -95,7 +95,7 @@ object IoTDeviceEventHandler {
                             status.result?.properties?.map { it.code to json.encodeToString(it.value) } ?: emptyList()
                         )
                     } catch (error: Throwable) {
-                        logger.error("Error updating value !! $iotDevice, $status, ${error.message}")
+                        logger.error("Error updating value !! $iotDevice, $status, ${error}")
                     }
                 } else {
                     logger.error("IoT device id not found for device-${devId} ${iotDevice}")
